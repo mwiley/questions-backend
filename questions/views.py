571bf50c6e192edbe80b9375c4730583b116fb56
Mason <masonwiley92@gmail.com>
2023-01-06 11:50:18 -0800

    sort newest questions first

--- questions/views.py
+++ questions/views.py
@@ -3,5 +3,5 @@ from questions.serializers import QuestionSerializer
 from rest_framework import viewsets
 
 class QuestionViewSet(viewsets.ModelViewSet):
-    queryset = Question.objects.all()
+    queryset = Question.objects.all().order_by('-pub_date')
     serializer_class = QuestionSerializer
